0e84a73fba4a676fda03b01247828e98cc06ba5b
waterseeker <wayneburris@Waynes-MacBook-Air.local>
2016-10-02 18:46:00 -0500

    added sum button and function but still can't get reroll to work

--- scripts.js
+++ scripts.js
@@ -1,17 +1,49 @@
 $(document).ready(function() {
     
-
+//create an Add A Die button
 var btn = document.createElement("button");
 var text = document.createTextNode("Add A Die");
 
-//create global counter to count dice on the page
-var counter = 0;
-                
 btn.addEventListener('click', addDie);
 btn.style.margin = '20px';
 btn.style.display = 'inline';
 btn.appendChild(text);
 document.body.appendChild(btn);
+
+//create a Roll Dice button
+
+var rollBtn = document.createElement("button");
+var rollBtnText = document.createTextNode("Roll Dice");
+rollBtn.addEventListener('click', rollAllDice);
+rollBtn.style.margin = '20px';
+rollBtn.style.display = 'inline';
+rollBtn.appendChild(rollBtnText);
+document.body.appendChild(rollBtn);
+
+//create a sum button
+var sumBtn = document.createElement("button");
+var sumText = document.createTextNode("Sum Dice");
+sumBtn.addEventListener('click', sumDie);
+sumBtn.style.margin = '20px';
+sumBtn.style.display = 'inline';
+sumBtn.appendChild(sumText);
+document.body.appendChild(sumBtn);
+
+  
+function sumDie() {
+   var currentDie = document.getElementsByClassName("Die");;
+   var total = 0;
+   for (var i = 0; i < currentDie.length; i++) {
+       total += currentDie[i].value;
+   }
+   alert("The sum of all the die is equal to " + total);
+}
+
+
+//create global counter to count dice on the page
+var counter = 0;
+                
+
 
 //adding die
 function addDie() {
@@ -20,25 +52,22 @@ function addDie() {
     var value = Math.floor((Math.random()*6)+1);
     die.className = "Die";
     die.style.color = 'white';
-    //modifying square
-    die.style.backgroundColor='black';
+    //styling die
+    die.style.backgroundImage="url('http://www.quarter-sawnflooring.com/images/curly-Cherry.jpg')";
     die.style.height='50px';
     die.style.width='50px';
     die.style.display="inline-block";
     die.style.margin='10px';
     die.style.textAlign='center';
     die.style.lineHeight='50px';
-    //die.value = Math.floor((Math.random()*6)+1);
+    die.value = Math.floor((Math.random()*6)+1);
     //increase counter everytime you addDie
     counter++;
     //assign counter as the id
     die.id = counter;
-    //assigns the class "dice" to the die
-    die.class = "dice";
-    die.innerHTML = 0;
     //put the die on the page
     document.body.appendChild(die);
-    //die.innerHTML = die.value;
+    die.innerHTML = die.value;
     // assign new value to the die when the roll die button is clicked.
         function rollDice () {
             var value = Math.floor((Math.random()*6)+1);
@@ -47,16 +76,22 @@ function addDie() {
         } // end of rollDice
             }; // end of addDie function
 
-function rollAllDice () {
-    var diceList = document.getElementsByClassName ("dice");
-    [].forEach.call(diceList, function (el) {
-        var value = Math.floor((Math.random()*6)+1);
-        this.innerHTML = this.value;});
-};//end of rollAllDice
+function rollAllDice (item,index,arr) {
+            var diceList = document.getElementsByClassName ("Die");
+            diceList[index] = Math.floor((Math.random()*6)+1);
+            this.innerHtml = Math.floor((Math.random()*6)+1);
+       
 
- document.getElementById("rollBtn").onclick = function() {rollAllDice()};       
-
-    
+// example of forEach loop
+// function myFunction(item,index,arr) {
+//     arr[index] = item * document.getElementById("multiplyWith").value;
+//     demo.innerHTML=numbers;
+//     }
+        
+    // for (i = 0; i < diceList.length; i++) {
+    // diceList[i].innerHtml = Math.floor((Math.random()*6)+1);
+    //     }
+};//end of rollAllDice      
 
 });///end of document.ready    
     
